a59457f8a6bf6a70aaf46559369c20dde88d2599
Julius Aka <julius.aka@uni-a.de>
2025-10-23 18:41:51 +0200

    regenerated FMUs with Code Export and Cross-Compile

--- generate_fmus.sh
+++ generate_fmus.sh
@@ -2,12 +2,12 @@
 # run this in git bash terminal to generate all fmus
 # git bash: bash generate_fmus.sh
 
-uv run export-fmu models/ClaRaTester/SteamCycle_01.mo ./models/ClaRaTester/libraries.txt --CopyExternalResources
-uv run export-fmu models/KoopmanTester/Tu_et_al/NonlinearModel.mo ./models/KoopmanTester/libraries.txt
-uv run export-fmu models/KoopmanTester/chaotic_lorenz_system/chaoticLorenz.mo ./models/KoopmanTester/libraries.txt
-uv run export-fmu models/SeriesResonanceTesters/SimpleSeriesResonance.mo ./models/SeriesResonanceTesters/libraries.txt
-uv run export-fmu models/SeriesResonanceTesters/SimpleSeriesResonanceTwoDyn.mo ./models/SeriesResonanceTesters/libraries.txt
-uv run export-fmu models/SeriesResonanceTesters/AdvancedSeriesResonance.mo ./models/SeriesResonanceTesters/libraries.txt
-uv run export-fmu models/ThermalTesters/StratifiedHeatFlowModel.mo ./models/ThermalTesters/libraries.txt
-uv run export-fmu models/ThermalTesters/ThreeStratifiedHeatFlowModels.mo ./models/ThermalTesters/libraries.txt
-uv run export-fmu models/MechanicalTesters/CartPole_polar.mo ./models/MechanicalTesters/libraries.txt
+uv run export-fmu models/ClaRaTester/SteamCycle_01.mo ./models/ClaRaTester/libraries.txt --CopyExternalResources --EnableCodeExport  --CrossExport
+uv run export-fmu models/KoopmanTester/Tu_et_al/NonlinearModel.mo ./models/KoopmanTester/libraries.txt --EnableCodeExport --CrossExport
+uv run export-fmu models/KoopmanTester/chaotic_lorenz_system/chaoticLorenz.mo ./models/KoopmanTester/libraries.txt --EnableCodeExport --CrossExport
+uv run export-fmu models/SeriesResonanceTesters/SimpleSeriesResonance.mo ./models/SeriesResonanceTesters/libraries.txt --EnableCodeExport --CrossExport
+uv run export-fmu models/SeriesResonanceTesters/SimpleSeriesResonanceTwoDyn.mo ./models/SeriesResonanceTesters/libraries.txt --EnableCodeExport --CrossExport
+uv run export-fmu models/SeriesResonanceTesters/AdvancedSeriesResonance.mo ./models/SeriesResonanceTesters/libraries.txt --EnableCodeExport --CrossExport
+uv run export-fmu models/ThermalTesters/StratifiedHeatFlowModel.mo ./models/ThermalTesters/libraries.txt --EnableCodeExport --CrossExport
+uv run export-fmu models/ThermalTesters/ThreeStratifiedHeatFlowModels.mo ./models/ThermalTesters/libraries.txt --EnableCodeExport --CrossExport
+uv run export-fmu models/MechanicalTesters/CartPole_polar.mo ./models/MechanicalTesters/libraries.txt --EnableCodeExport --CrossExport
